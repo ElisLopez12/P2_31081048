6fbd2231b73c8cb46e0ebf7c999e505e06d46ee0
Elis Lopez <elismiguellopezgonzalez@hotmail.com>
2024-06-01 13:53:46 -0400

    Sin terminar

--- controllers/ContactosController.js
+++ controllers/ContactosController.js
@@ -1,12 +1,35 @@
 const ContactosModel = require("../models/ContactosModel");
 
+
 class ContactosController {
   constructor() {
     this.contactosModel = new ContactosModel();
     this.add = this.add.bind(this);
   }
 
-  async add(req, res) {
+  async obtenerIp() {
+    try {
+      const response = await fetch('https://api.ipify.org?format=json');
+      const data = await response.json();
+      return data.ip; 
+    } catch (error) {
+      console.error('Error al obtener la ip:', error);
+      return null; 
+    }
+  }
+
+  async obtenerPais(ip) {
+    try {
+      const response = await fetch('https://ipinfo.io/'+ip+'?token=8ce00bc8d3bc24');
+      const data = await response.json();
+      return data.country; 
+    } catch (error) {
+      console.error('Error al obtener el país:', error);
+      return null; 
+    }
+  }
+
+    async add(req, res) {
     // Validar los datos del formulario
 
     const { email, name, mensaje } = req.body;
@@ -17,11 +40,12 @@ class ContactosController {
     }
 
     // Guardar los datos del formulario
-    const ip = req.ip;
+    const ip = await this.obtenerIp();
     const fecha = new Date().toISOString();
+    const pais = await this.obtenerPais(ip);
 
    
-    await this.contactosModel.crearContacto(email, name, mensaje, ip, fecha);
+    await this.contactosModel.crearContacto(email, name, mensaje, ip, fecha, pais);
 
     const contactos = await this.contactosModel.obtenerAllContactos();
 
--- models/ContactosModel.js
+++ models/ContactosModel.js
@@ -2,6 +2,7 @@ const sqlite3 = require("sqlite3").verbose();
 const { error } = require("console");
 const {promisify}= require("util");
 
+
 class ContactosModel {
     constructor() {
   
@@ -14,7 +15,7 @@ class ContactosModel {
       });
   
       this.db.run(
-        "CREATE TABLE IF NOT EXISTS contactos (email TEXT, nombre TEXT, mensaje TEXT, ip TEXT, fecha TEXT, id INTEGER PRIMARY KEY AUTOINCREMENT)",
+        "CREATE TABLE IF NOT EXISTS contactos (email TEXT, nombre TEXT, mensaje TEXT, ip TEXT, fecha TEXT,pais text, id INTEGER PRIMARY KEY AUTOINCREMENT)",
         (err) => {
           if (err) {
             console.error(err.message);
@@ -23,10 +24,11 @@ class ContactosModel {
       );
     }
   
-    crearContacto(email, nombre, mensaje, ip, fecha) {
+
+    crearContacto(email, nombre, mensaje, ip, fecha, pais) {
       return new Promise((resolve, reject) => {
-        const sql = `INSERT INTO contactos (email, nombre, mensaje, ip, fecha) VALUES (?, ?, ?, ?, ?)`;
-        this.db.run(sql, [email, nombre, mensaje, ip, fecha], function (err) {
+        const sql = `INSERT INTO contactos (email, nombre, mensaje, ip, fecha,pais) VALUES (?, ?, ?, ?, ?,?)`;
+        this.db.run(sql, [email, nombre, mensaje, ip, fecha, pais], function (err) {
           if (err) {
             console.error(err.message);
             reject(err);
